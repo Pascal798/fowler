bdde806235b3926c5576c834b585b332753726f6
Pascal798 <PascalStephan2016@gmail.com>
2020-05-14 13:50:07 +0200

    rename amountFor

--- src/main/Customer.java
+++ src/main/Customer.java
@@ -25,7 +25,7 @@ class Customer {
             double thisAmount = 0;
             Rental each = (Rental) enum_rentals.nextElement();
             //determine amounts for each line
-            thisAmount = each.amountFor();
+            thisAmount = amountFor(each);
             // add frequent renter points
             frequentRenterPoints ++;
             // add bonus for a two day new release rental
@@ -40,6 +40,11 @@ class Customer {
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
         return result;
     }
+
+    private double amountFor(Rental aRental) {
+        return aRental.getCharge();
+    }
+
 
 }
     
