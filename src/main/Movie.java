9a827281ba82fa160ecd9489fa6c9c7a5a674c74
Pascal798 <PascalStephan2016@gmail.com>
2020-05-18 09:21:55 +0200

    create new Clases and Move getFrequentRenterPoint and getCharge

--- src/main/Movie.java
+++ src/main/Movie.java
@@ -3,45 +3,41 @@ public class Movie {
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
     private String title;
-    private int priceCode;
+    private Price priceCode;
     public Movie(String newtitle, int newpriceCode) {
         title = newtitle;
         setPriceCode(newpriceCode);
     }
     public int getPriceCode() {
-        return priceCode;
+        return priceCode.getPriceCode();
     }
     public void setPriceCode(int arg) {
-        priceCode = arg;
+        switch (arg) {
+            case REGULAR:
+                priceCode = new RegularPrice();
+                break;
+            case CHILDRENS:
+                priceCode = new ChildrensPrice();
+                break;
+            case NEW_RELEASE:
+                priceCode = new NewReleasePrice();
+                break;
+            default:
+                throw new IllegalArgumentException("Incorrect Price Code");
+        }
+
     }
     public String getTitle (){
         return title;
     };
 
-    double getCharge(int daysRented) {
-        double result = 0;
-        switch (getPriceCode()) {
-            case REGULAR:
-                result += 2;
-                if (daysRented > 2)
-                    result += (daysRented - 2) * 1.5;
-                break;
-            case NEW_RELEASE:
-                result += daysRented * 3;
-                break;
-            case CHILDRENS:
-                result += 1.5;
-                if (daysRented > 3)
-                    result += (daysRented - 3) * 1.5;
-                break;
-        }
-        return result;
+    double getCharge(int daysRented) { return
+            priceCode.getCharge(daysRented);
     }
 
+
     int getFrequentRenterPoints(int daysRented) {
-        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) return 2;
-        else
-            return 1;
+        return priceCode.getFrequentRenterPoints(daysRented);
     }
 
 }
