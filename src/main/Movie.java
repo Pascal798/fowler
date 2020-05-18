fe5ad1706b35d52ac8d5faf8594ca39452974e8c
Pascal798 <PascalStephan2016@gmail.com>
2020-05-18 09:03:56 +0200

    move getTotalFrequentPoints to Movie

--- src/main/Movie.java
+++ src/main/Movie.java
@@ -6,7 +6,7 @@ public class Movie {
     private int priceCode;
     public Movie(String newtitle, int newpriceCode) {
         title = newtitle;
-        priceCode = newpriceCode;
+        setPriceCode(newpriceCode);
     }
     public int getPriceCode() {
         return priceCode;
@@ -38,5 +38,10 @@ public class Movie {
         return result;
     }
 
+    int getFrequentRenterPoints(int daysRented) {
+        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) return 2;
+        else
+            return 1;
+    }
 
 }
